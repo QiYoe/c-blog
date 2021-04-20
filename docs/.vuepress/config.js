8a4363586bc2ee0c72254665fe0c49d3fc2174b5
qiyao-web <2328715018@qq.com>
2021-04-20 14:26:27 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
   title: '平光眼镜',
   description: "Don't forget, a person's greatest emotional need is to feel appreciated.",
   dest: 'public',
+  base: '/c-blog/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
