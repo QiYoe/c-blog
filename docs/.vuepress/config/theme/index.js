e4e9a680bb7e23c179cdc8d82a6f0b4e23546cb9
qiyao-web <2328715018@qq.com>
2021-04-20 15:02:20 +0800

    add comment

--- docs/.vuepress/config/theme/index.js
+++ docs/.vuepress/config/theme/index.js
@@ -11,4 +11,16 @@ module.exports = Object.assign({}, themeReco, {
   searchMaxSuggestions: 10,
   // 自动形成侧边导航
   sidebar: 'auto',
+  valineConfig: {
+    appId: 'GKPfReaObN41kUWNy1ktUOAg-gzGzoHsz',// your appId
+    appKey: 'JQUswxYaCmzNPJasurnphUS3', // your appKey
+    avatar: 'wavatar' // (''/mp/identicon/monsterid/wavatar/robohash/retro/hide)
+  },
+  vssueConfig: {
+    platform: 'github',
+    // owner: 'OWNER_OF_REPO',
+    // repo: 'NAME_OF_REPO',
+    // clientId: 'YOUR_CLIENT_ID',
+    // clientSecret: 'YOUR_CLIENT_SECRET',
+  }
 })
