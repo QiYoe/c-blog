71d45cb175eb8419c2855d2f1054cbdcf79475a4
QiQao <36325188+QiYoe@users.noreply.github.com>
2021-05-11 23:38:23 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,28 +8,28 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-    ['script', {}, `
-      var _hmt = _hmt || [];
-      (function() {
-      var hm = document.createElement("script");
-      hm.src = "https://hm.baidu.com/hm.js?**********************";
-      var s = document.getElementsByTagName("script")[0]; 
-      s.parentNode.insertBefore(hm, s);
+//     ['script', {}, `
+//       var _hmt = _hmt || [];
+//       (function() {
+//       var hm = document.createElement("script");
+//       hm.src = "https://hm.baidu.com/hm.js?**********************";
+//       var s = document.getElementsByTagName("script")[0]; 
+//       s.parentNode.insertBefore(hm, s);
 
-      // 引入谷歌,不需要可删除这段
-      var hm1 = document.createElement("script");
-      hm1.src = "https://www.googletagmanager.com/gtag/js?id=UA-00000000-1";
-      var s1 = document.getElementsByTagName("script")[0]; 
-      s1.parentNode.insertBefore(hm1, s1);
-      })();
+//       // 引入谷歌,不需要可删除这段
+//       var hm1 = document.createElement("script");
+//       hm1.src = "https://www.googletagmanager.com/gtag/js?id=UA-00000000-1";
+//       var s1 = document.getElementsByTagName("script")[0]; 
+//       s1.parentNode.insertBefore(hm1, s1);
+//       })();
 
-      // 谷歌加载,不需要可删除
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+//       // 谷歌加载,不需要可删除
+//       window.dataLayer = window.dataLayer || [];
+//       function gtag(){dataLayer.push(arguments);}
+//       gtag('js', new Date());
 
-      gtag('config', 'UA-00000000-1');
-    `]
+//       gtag('config', 'UA-00000000-1');
+//     `]
   ],
   theme: 'reco',
   themeConfig,
@@ -43,11 +43,11 @@ module.exports = {
   plugins: [
     '@vuepress/medium-zoom',
     'flowchart',
-    [
-      '@vuepress/google-analytics',
-      {
-        'ga': 'UA-149666038-1' // UA-00000000-0
-      }
-    ]
+//     [
+//       '@vuepress/google-analytics',
+//       {
+//         'ga': 'UA-149666038-1' // UA-00000000-0
+//       }
+//     ]
   ]
 }
