482d48fb94b9ea0ad195f5e3b05e040098f0dfaf
QiQao <36325188+QiYoe@users.noreply.github.com>
2021-05-12 15:54:32 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,11 +8,7 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'baidu-site-verification', content: 'code-GwygrEuKwm'}],
-    ['meta', { name: 'keywords', content: 'vuepress'}],
-    ['meta', { name: 'keywords', content: 'blog'}],
-    ['meta', { name: 'keywords', content: '博客'}],
-    ['meta', { name: 'keywords', content: '南上康青山'}],
-    ['meta', { name: 'keywords', content: '狗窝'}],
+    ['meta', { name: 'keywords', content: '南上康青山, vuepress, blog, 博客, 狗窝'}],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['meta', { name: 'baidu_union_verify', content: 'acd0eb90461e19817cabd1140c8ffd54' }],
     [
