880890dfcab85cc1ac44c5c4942044576ff308d5
QiQao <36325188+QiYoe@users.noreply.github.com>
2021-05-12 18:29:44 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
 //   base: '/c-blog/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'baidu-site-verification', content: 'code-GwygrEuKwm'}],
+    ['meta', { name: 'baidu-site-verification', content: 'code-5BMDhSDgSq'}],
     ['meta', { name: 'keywords', content: '南上康青山, vuepress, blog, 博客, 狗窝'}],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['meta', { name: 'baidu_union_verify', content: 'acd0eb90461e19817cabd1140c8ffd54' }],
