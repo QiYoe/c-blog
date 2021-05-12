94c68523e20306181624cc505030833955e4f975
QiQao <36325188+QiYoe@users.noreply.github.com>
2021-05-12 17:49:36 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,21 +27,8 @@ module.exports = {
         hm.src = "https://hm.baidu.com/hm.js?8dbec0e055b85f45ae350a0bfcb52202";
         var s = document.getElementsByTagName("script")[0]; 
         s.parentNode.insertBefore(hm, s);
-
-//       // 引入谷歌,不需要可删除这段
-//       var hm1 = document.createElement("script");
-//       hm1.src = "https://www.googletagmanager.com/gtag/js?id=UA-00000000-1";
-//       var s1 = document.getElementsByTagName("script")[0]; 
-//       s1.parentNode.insertBefore(hm1, s1);
-       })();
-
-//       // 谷歌加载,不需要可删除
-//       window.dataLayer = window.dataLayer || [];
-//       function gtag(){dataLayer.push(arguments);}
-//       gtag('js', new Date());
-
-//       gtag('config', 'UA-00000000-1');
-     `]
+      })();
+    `]
   ],
   theme: 'reco',
   themeConfig,
